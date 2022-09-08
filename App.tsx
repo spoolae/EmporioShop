d98536a5424f00cc7511c089197e7529fad4ea33
Spoolae <ohotnic147feed@gmail.com>
2022-09-08 09:28:31 +0200

    Project Init

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>123</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
